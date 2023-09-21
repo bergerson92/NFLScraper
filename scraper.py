3b5132d80fdcfe029c5c3b77c4ecc0cc5af7c2d1
bergerson92 <90724387+bergerson92@users.noreply.github.com>
2023-09-21 15:47:22 +0200

    Write to Gsheet

--- scraper.py
+++ scraper.py
@@ -1,22 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import pygsheets
 
-response = requests.get(url="https://en.wikipedia.org/wiki/List_of_Super_Bowl_champions")
+response = requests.get(
+    url="https://en.wikipedia.org/wiki/List_of_Super_Bowl_champions"
+)
 
-#print(response.status_code)
+# print(response.status_code)
 
-soup = BeautifulSoup(response.content, 'html.parser')
+soup = BeautifulSoup(response.content, "html.parser")
 
-title = soup.find(id="firstHeading")
+# title = soup.find(id="firstHeading")
 
-#print(title.string)
+# print(title.string)
 
-championstable = soup.find('table', {'class':"wikitable sortable"})
+championstable = soup.find("table", {"class": "wikitable sortable"})
 
-#print(championstable)
+# print(championstable)
 
 df = pd.read_html(str(championstable))
-df=pd.DataFrame(df[0])
-#print(df)
+df = pd.DataFrame(df[0])
+# print(df)
 
+gc = pygsheets.authorize(
+    service_file="/Users/pathtoFile"
+)
+
+# df = pd.DataFrame()
+
+# df['name'] = ['John', 'Sarah', 'Steve']
+
+sh = gc.open("NFL Champions")
+
+wks = sh[0]
+
+wks.set_dataframe(df, (1, 1))
